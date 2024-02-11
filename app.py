f1e6070edb836f60d99cb69b0a54805cf5d4558e
Eddie Sandler <ed.sandler@gmail.com>
2024-02-11 16:01:22 -0500

    updated app for render

--- app.py
+++ app.py
@@ -14,32 +14,7 @@ from models import db, User,Watchlist # Import the models
 from forms import RegisterForm,LoginForm # Import the form
 import datetime
 
-import boto3
-from botocore.exceptions import ClientError
 
-
-def get_secret():
-
-    secret_name = "myAPIkeys"
-    region_name = "us-east-1"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
-
-    secret = get_secret_value_response['SecretString']
 
 
 
@@ -50,7 +25,7 @@ FRED_API_KEY=os.environ['FRED_API_KEY']
 WEATHER_API_KEY=os.environ['WEATHER_API_KEY']
 OPENAI_API_KEY=os.environ['OPENAI_API_KEY']
 
-# fred =Fred(api_key=os.environ['FRED_API_KEY'])
+fred =Fred(api_key=os.environ['FRED_API_KEY'])
 print(os.environ)
 client = openai.Client(api_key=OPENAI_API_KEY)
 
